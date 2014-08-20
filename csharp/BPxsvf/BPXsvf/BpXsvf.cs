17ed2bae39613ef9a09579d0dc93024ec60c0ed5
7 <noobwebdev666@gmail.com>
2014-08-20 19:50:31 +0800

    added new file and on going development

---
+++ csharp/BPxsvf/BPXsvf/BpXsvf.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO.Ports;
+
+namespace BPXsvf
+{
+
+    public interface IOutDisplay
+    {
+        void Display(string str);
+
+    }
+
+
+    public class BpXsvfParameterData
+    {
+        public int ComBaudRate { get; set; }
+        public string ComName { get; set; }
+        public bool ResetJtag { get; set; }
+        public bool ChainScan { get; set; }
+        public string XsvfFilePath { get; set; }
+    }
+    
+
+    public abstract class AbstractBpXsvf
+    {
+        public const int JTAG_RESET= 0x01;
+        public const int JTAG_CHAIN_SCAN= 0x02;
+        public const int XSVF_PLAYER= 0x03;
+
+
+        public const int XSVF_ERROR_NONE= 0x00;
+        public const int XSVF_ERROR_UNKNOWN= 0x01;
+        public const int XSVF_ERROR_TDOMISMATCH= 0x02;
+        public const int XSVF_ERROR_MAXRETRIES= 0x03;
+        public const int XSVF_ERROR_ILLEGALCMD= 0x04;
+        public const int XSVF_ERROR_ILLEGALSTATE= 0x05;
+        public const int XSVF_ERROR_DATAOVERFLOW= 0x06;
+        public const int XSVF_ERROR_LAST= 0x07;
+        public const int XSVF_READY_FOR_DATA = 0xFF;
+
+
+        //protected SerialPort mSerial;
+        IOutDisplay mIoutdisplay;
+        protected BpXsvfParameterData mParamData;
+
+        //public void SetSerialPort(SerialPort s)
+        //{
+        //    mSerial = s;
+        //}
+
+        public void SetOutDisplay(IOutDisplay i)
+        {
+
+            mIoutdisplay = i;
+        }
+
+        public void Greet()
+        {
+            mIoutdisplay.Display("BP XSVF in C# by 7");
+        }
+
+
+        public void SetBpXsvfParameterData(BpXsvfParameterData paramData)
+        {
+            mParamData = paramData;
+
+        }
+
+        public abstract void DoProcess();
+
+    }
+
+
+
+
+    public class BpXsvfFork:AbstractBpXsvf
+    {
+
+        public override void DoProcess()
+        {
+            BpXsvfParameterData paramdata = mParamData;
+            SerialPort serialport = new SerialPort(paramdata.ComName,paramdata.ComBaudRate,Parity.None,8,StopBits.One);
+
+            
+
+
+        }
+
+
+
+    }
+
+}
--- csharp/BPxsvf/BPXsvf/Program.cs
+++ csharp/BPxsvf/BPXsvf/Program.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
 
 namespace BPXsvf
 {
-    class Program
+    class Program : IOutDisplay
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.MainProcess();
+        }
+
+        public void MainProcess()
+        {
+            BpXsvfParameterData bpParamData = new BpXsvfParameterData();
+            bpParamData.ChainScan = true;
+            bpParamData.ComBaudRate = 115200;
+            bpParamData.ComName = "COM1";
+            bpParamData.ResetJtag = true;
+            bpParamData.XsvfFilePath = "c:\\out.txt";
+
+            //SerialPort ser = new SerialPort();
+
+            AbstractBpXsvf iXsvf = new BpXsvfFork();
+            iXsvf.SetBpXsvfParameterData(bpParamData);
+            //iXsvf.SetSerialPort(ser);
+            iXsvf.SetOutDisplay(this);
+            iXsvf.Greet();
+
+
+            Console.ReadKey();
+        }
+
+        // IOutDisplay interface
+        public void Display(string str)
+        {
+            Console.WriteLine(str);
         }
     }
 }
